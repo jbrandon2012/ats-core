8ea1d32f8e6c82aa62543142838b1b337ccf85d8
jbrandon2012 <jonathan.br.garcia@gmail.com>
2024-04-14 20:13:49 -0400

    background and other progress

--- settings.py
+++ settings.py
@@ -3,13 +3,26 @@ from pygame.math import Vector2
 # screen size
 SCREEN_WIDTH = 1280
 SCREEN_HEIGHT = 720
+TILE_SIZE = 64
 
 # overlay positions
 OVERLAY_POSITIONS = {
     'tool' : (40, SCREEN_HEIGHT - 30),
     'seed' : (100, SCREEN_HEIGHT - 30)}
 
-
+LAYERS = {
+    'water': 0,
+    'ground': 1,
+    'soil': 2,
+    'soil water': 3,
+    'rain floor': 4,
+    'house bottom': 5,
+    'ground plant': 6,
+    'main': 7,
+    'house top': 8,
+    'fruit': 9,
+    'rain drops': 10
+}
 
 
 
